cc49ffbbca1d9164764b2de7e7972f56aa1b1285
wph <1315306351@qq.com>
2019-08-16 08:50:52 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import EmpList from '@/components/EmpList'
+import EmpAdd from '@/components/EmpAdd'
+import EmpUpdate from '@/components/EmpUpdate'
+import EmpUpload from '@/components/EmpUpload'
+import upload from '@/components/upload'
+
+
+Vue.use(Router)
+
+export default new Router({
+	mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'EmpList',
+      component: EmpList
+    },
+    {
+    	path:'/EmpAdd',
+    	name:'EmpAdd',
+    	component:EmpAdd
+    },
+    {
+    	path:'/EmpUpdate',
+    	name:'EmpUpdate',
+    	component:EmpUpdate
+    },
+    {
+    	path:'/EmpUpload',
+    	name:'EmpUpload',
+    	component:EmpUpload
+    },
+    {
+    	path:'/upload',
+    	name:'upload',
+    	component:upload
+    }
+  ]
+})
